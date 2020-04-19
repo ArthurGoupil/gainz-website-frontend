86b9b8ba476019a658c884b342ad70992d6ffe57
ArthurGoupil <arthurgoupil@gmail.com>
2020-04-20 00:00:45 +0200

    improve picture finitions

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "39bacf58885fd4322c36";
+/******/ 	var hotCurrentHash = "59fe056b4c42ea7ffd29";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
