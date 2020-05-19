fdbf849ea705b761b8573d4cc22d394901b074a3
ArthurGoupil <arthurgoupil@gmail.com>
2020-05-20 00:50:57 +0200

    correct package.json

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@ module.exports = {
   env: {
     BACKEND_URL: 'https://gainz-website.herokuapp.com',
   },
-  scripts: {
-    build: 'next build && next export',
-  },
   webpack: (config, { isServer }) => {
     if (isServer) {
       require('./generate-sitemap');
