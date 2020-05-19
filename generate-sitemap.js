11b36a77b799dcb7696fe1b3c2fe1d54ecccfa57
ArthurGoupil <arthurgoupil@gmail.com>
2020-05-20 00:55:44 +0200

    remove sitemap management

--- generate-sitemap.js
+++
@@ -1,8 +0,0 @@
-const sitemap = require('nextjs-sitemap-generator');
-
-sitemap({
-  baseUrl: 'https://gainz-art.com',
-  pagesDirectory: __dirname + '/pages',
-  targetDirectory: 'public/',
-  nextConfigPath: __dirname + '/next.config.js',
-});
--- next.config.js
+++ next.config.js
@@ -2,11 +2,4 @@ module.exports = {
   env: {
     BACKEND_URL: 'https://gainz-website.herokuapp.com',
   },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      require('./generate-sitemap');
-    }
-
-    return config;
-  },
 };
